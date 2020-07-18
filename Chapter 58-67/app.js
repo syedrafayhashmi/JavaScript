b8c31991c81468951cef08028dc1e248a8b50894
syedrafayhashmi <rafaysidsid@gmail.com>
2020-07-19 01:54:52 +0500

    58-67 done

--- Chapter 58-67/app.js
+++ Chapter 58-67/app.js
@@ -13,3 +13,30 @@ document.getElementById("last-name").value = "HASHMI"
 document.getElementById("email").value = "abc@gmail.com"
 
 // Task 2
+var form = document.getElementById("form-content");
+if(form.nodeType === 1){
+    document.write("<br> nodeType of form-content is 1 which means tags like div or p");
+}
+else if(form.nodeType === 3){
+    document.write("<br> nodeType of form-content is 3 which means text");
+
+}
+var lname = document.getElementById("lastName");
+if(lname.nodeType === 1){
+    document.write("<br> nodeType of lastName is 1 which means tags like div or p");
+}
+else if(lname.nodeType === 3){
+    document.write("<br> nodeType of lastName is 3 which means text");
+}
+for(var z=0;z<lname.children.length;z++){
+    document.write(lname.children[x].innerHTML); 
+}
+// no child nodes 
+var fChild = main.firstChild;
+var lChile = main.lastChild;
+var nextSiblings = lname.nextSibling;
+var previousSiblings = lname.previousSibling;
+
+var email = document.getElementById("email");
+var emailParentNode  = email.parentNode;
+var nodeTypeEmail = email.nodeType;
